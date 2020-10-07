ef2f41d1736b83b969f250efbeb4659ed2289c42
Andrey862 <f_a_m@list.ru>
2020-10-07 11:00:17 +0300

    add client read-write

--- Client/client.py
+++ Client/client.py
@@ -1,3 +1,10 @@
+def send_file_to_dn(dn_ip, content, port=10005):
+    socket = socket.socket()
+    socket.connect((dn_ip, port))
+    socket.sendall(b'write\n2\n2\nf\n'+str(len(content)).encode('UTF-8')+b'\n')
+    socket.recv(1024)
+
+
 def ask_client():
     pass
 
--- DataNode/data_node.py
+++ DataNode/data_node.py
@@ -5,17 +5,107 @@ import time
 import os
 
 connection_to_nn = None
-content_table = None
+content_table = {1: [1, False]}
 dn_status_table = None
+
+dn_client_port = 10005
+
+
+def get_chank_name(chank):
+    return f'__files__/{chank}.chank'
+
+
+to_bool = {
+    b'True': True,
+    b'true': True,
+    b't': True,
+    b'False': False,
+    b'false': False,
+    b'f': False,
+}
+
+
+class ClientServer(Thread):
+    def __init__(self, sock: socket.socket, addr) -> None:
+        super().__init__(daemon=True)
+        self.sock = sock
+        self.addr = addr
+
+    def recv_word(self):
+        word = b""
+        for _ in range(20):
+            word += self.sock.recv(1)
+            if word[-1] == b'\n'[0]:
+                break
+        return word[:-1]
+
+    def read(self, chank, version):
+        if (chank in content_table and content_table[chank][0] == version and not content_table[chank][1]):
+            with open(get_chank_name(chank), 'rb') as f:
+                self.sock.sendall(f.read())
+        else:
+            self.sock.sendall(b'No data\n')
+
+    def write(self, chank, version, deleted, length):
+        if (chank not in content_table or content_table[chank][0] <= version):
+            self.sock.sendall(b'ACK\n')
+        else:
+            self.sock.sendall(b'Too old\n')
+            return
+        if (deleted):
+            f_name = get_chank_name(chank)
+            if os.path.isfile(f_name):
+                os.remove(f_name)
+            self.sock.sendall(b'ACK\n')
+        else:
+            content = b''
+            while(len(content) < length):
+                part = self.sock.recv(1024)
+                content += part
+            with open(get_chank_name(chank), 'wb') as f:
+                f.write(content)
+            self.sock.sendall(b'ACK\n')
+
+        content_table[chank] = [version, deleted]
+
+    def serve(self):
+        command = self.recv_word()
+        print(command)
+        if (command == b'read'):
+            chank = int(self.recv_word())
+            version = int(self.recv_word())
+            self.read(chank, version)
+        elif (command == b'write'):
+            chank = int(self.recv_word())
+            version = int(self.recv_word())
+            deleted = to_bool[self.recv_word()]
+            length = int(self.recv_word())
+            self.write(chank, version, deleted, length)
+        else:
+            self.sock.sendall(b'unknown command "'+command+b'"\n')
+
+    def run(self):
+        try:
+            self.serve()
+        except Exception as e:
+            self.sock.sendall(b'Error! '+str(e).encode('UTF-8')+b'\n')
+        self.sock.close()
+        print(f'serverd {self.addr}')
 
 
 class ClientListener(Thread):
-    def __init__(self, sock: socket.socket) -> None:
+    def __init__(self) -> None:
         super().__init__(daemon=True)
-        self.sock = sock
 
     def action(self):
-        pass
+        client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        client_sock.bind(('', dn_client_port))
+        client_sock.listen()
+        print('Client Listener working')
+        while(True):
+            con, addr = client_sock.accept()
+            print(f'connected to {addr}')
+            ClientServer(con, addr).start()
 
     def run(self):
         while (True):
@@ -63,27 +153,6 @@ if __name__ == "__main__":
     # init_global_variables
     # read lab6 https://gist.github.com/gordinmitya/349f4abdc6b16dc163fa39b55544fd34
     # my solution to lab6 https://github.com/Andrey862/repo
-    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    nn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    nn_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    dn_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    dn_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    client_sock.bind(('', 8800))
-    client_sock.listen()
-    nn_sock.bind(('', 8801))
-    nn_sock.listen()
-    dn_sock.bind(('', 8802))
-    dn_sock.listen()
-    while(True):
-        condition = True
-        if (condition):
-            sock = client_sock
-        elif (condition):
-            sock = nn_sock
-        elif (condition):
-            sock = dn_sock
-
-        con, addr = sock.accept()
-        print('something happend')
-        ClientListener(con).start()
+    cl = ClientListener()
+    cl.start()
+    cl.join()
